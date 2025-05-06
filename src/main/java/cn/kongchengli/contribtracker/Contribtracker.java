488891152bdc6b57da6e03747708082b613918a2
chen <chenchen71956@gmail.com>
2025-05-06 17:35:02 +0800

    基础框架

---
+++ src/main/java/cn/kongchengli/contribtracker/Contribtracker.java
@@ -0,0 +1,10 @@
+package cn.kongchengli.contribtracker;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Contribtracker implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
